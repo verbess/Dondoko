9573eac36f775e72daddfc5fb8a8f6ad10acbf82
王世民 <verbess@foxmail.com>
2022-11-05 03:41:01 +0800

    feat: add core modules (#3)
    * feat: add Module class
    
    * feat: add exceptions (#2)
    
    * feat: add SR static class
    
    * feat: add DondokoException class
    
    * feat: add IReleasable interface (#4)

---
+++ src/Core/Exceptions/SR.cs
@@ -0,0 +1,11 @@
+namespace Dondoko
+{
+    internal static class SR
+    {
+        #region Dondoko exception
+
+        public const string Dondoko_FrameworkException = "Dondoko framework error.";
+
+        #endregion
+    }
+}
