e67d641acc2241ba74fb951b2dc4a5d800bc6035
verbess <verbess@foxmail.com>
2022-11-05 02:59:00 +0800

    feat: add Module class

---
+++ src/Core/Module.cs
@@ -0,0 +1,19 @@
+﻿namespace Dondoko
+{
+    internal abstract class Module
+    {
+        public const int DefaultPriority = 0;
+
+        protected Module() { }
+
+        /// <summary>
+        /// Gets the priority of the framework module.
+        /// </summary>
+        /// <remarks>Modules with higher priority will be updated earlier.</remarks>
+        public virtual int Priority => DefaultPriority;
+
+        public abstract void Update(float logicalElapse, float realElapse);
+
+        public abstract void Shutdown();
+    }
+}
