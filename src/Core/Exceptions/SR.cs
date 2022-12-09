0d8a95d2ced37abb02c83dbae7d63d6e642f9ac6
verbess <verbess@foxmail.com>
2022-12-09 08:55:47 +0800

    feat: add SR class

---
+++ src/Core/Exceptions/SR.cs
@@ -0,0 +1,14 @@
+namespace Dondoko;
+
+internal static class SR
+{
+    #region Dondoko exceptions
+
+    public const string Dondoko_FrameworkException = "Dondoko framework error.";
+
+    #endregion
+
+    #region Invalid operation exceptions
+
+    #endregion
+}
