9023c77bb347bff971fe9707c2dc1c57d95c30f2
王世民 <verbess@foxmail.com>
2022-12-13 12:34:01 +0800

    feat: add version module (#11)
    * feat: add Version class
    
    * feat: add IVersioner interface
    
    * refactor: simplify codes

---
+++ src/Core/Version/Version.cs
@@ -0,0 +1,19 @@
+namespace Dondoko;
+
+public static partial class Version
+{
+    private const string DondokoVersionString = "2022.12.13";
+    private const int DefaultInternalGameVersion = 0;
+
+    private static IVersioner? s_versioner;
+
+    static Version() => s_versioner = null;
+
+    public static string DondokoVersion => DondokoVersionString;
+
+    public static string GameVersion => s_versioner?.GameVersion ?? string.Empty;
+
+    public static int InternalGameVersion => s_versioner?.InternalGameVersion ?? DefaultInternalGameVersion;
+
+    public static void SetVersioner(IVersioner? versioner) => s_versioner = versioner;
+}
