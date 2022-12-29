c334e29181cd972724c20d5bb3971c9b074034ea
verbess <shiminw@outlook.com>
2022-12-29 17:32:35 +0800

    feat: add SR items

--- src/Core/Exceptions/SR.cs
+++ src/Core/Exceptions/SR.cs
@@ -10,5 +10,12 @@ internal static class SR
 
     #region Invalid operation exceptions
 
+    public const string InvalidOperation_InstanceCreationFailed
+        = "Failed to create a instance.";
+    public const string InvalidOperation_ReferenceReleased
+        = "The reference has already been released.";
+    public const string InvalidOperation_TypeMismatch
+        = "A mismatch of the generic type with the reference type.";
+
     #endregion
 }
