cabd4c66a065e1a6be64796c101aa0fdff966c0d
王世民 <shiminw@outlook.com>
2022-12-29 10:44:26 +0800

    style: break up long lines (#15)
    * style: break up long lines
    
    * style: break up long lines
    
    * style: break up long lines
    
    * style: break up long lines
    
    * style: break up long lines
    
    * style: break up long lines

--- src/Utility/TextFormatter.IFormatter.cs
+++ src/Utility/TextFormatter.IFormatter.cs
@@ -8,7 +8,8 @@ public static partial class TextFormatter
 
         public string Format(string format, object? arg0, object? arg1);
 
-        public string Format(string format, object? arg0, object? arg1, object? arg2);
+        public string Format(
+            string format, object? arg0, object? arg1, object? arg2);
 
         public string Format(string format, params object?[] args);
     }
